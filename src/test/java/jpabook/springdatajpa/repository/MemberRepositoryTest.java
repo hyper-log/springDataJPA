282c3216bc7663a98eb2ef4f9da4a9bd71002d5c
hyper <revoluer9@gmail.com>
2025-01-03 22:59:52 +0900

    Query By Example (비추천/left join 불가능)

--- src/test/java/jpabook/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/springdatajpa/repository/MemberRepositoryTest.java
@@ -312,22 +312,26 @@ class MemberRepositoryTest {
     // 비추천
     @Test
     public void queryByExample() {
+
         //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
         em.persist(new Member("m1", 0, teamA));
         em.persist(new Member("m2", 0, teamA));
         em.flush();
+
         //when
         //Probe 생성
         Member member = new Member("m1");
         Team team = new Team("teamA"); //내부조인으로 teamA 가능
         member.setTeam(team);
+
         //ExampleMatcher 생성, age 프로퍼티는 무시
         ExampleMatcher matcher = ExampleMatcher.matching()
                 .withIgnorePaths("age");
         Example<Member> example = Example.of(member, matcher);
         List<Member> result = memberRepository.findAll(example);
+
         //then
         assertThat(result.size()).isEqualTo(1);
     }
