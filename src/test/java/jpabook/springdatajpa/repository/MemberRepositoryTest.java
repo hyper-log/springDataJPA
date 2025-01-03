b35450afa82acfe8b4bc993cf9a4293439bc6357
hyper <revoluer9@gmail.com>
2025-01-03 22:46:24 +0900

    Query By Example (비추천/left join 불가능)

--- src/test/java/jpabook/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/springdatajpa/repository/MemberRepositoryTest.java
@@ -9,10 +9,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -310,8 +307,36 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findAll(spec);
 
         Assertions.assertThat(result.size()).isEqualTo(1);
-
     }
 
+    @Test
+    public void queryByExample() {
 
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 10, teamA);
+        Member m2 = new Member("m2", 10, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member member = new Member("m1");
+        Team teamB = new Team("teamA");
+        member.setTeam(teamB);
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("age");
+
+        // innerJoin 가능, left join 불가능
+        Example<Member> example = Example.of(member);
+
+        List<Member> result = memberRepository.findAll(example);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("m1");
+    }
 }
